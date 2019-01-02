2ddc58cdcb00536728ff395041cc09db3bcae094
libin1991 <3120217729@qq.com>
2019-01-02 21:31:14 +0800

    l

---
+++ README.md
@@ -0,0 +1,5 @@
+# &lt;vue-clamp&gt;
+
+Clamping multiline text with ease.
+
+See more in our [docs & demo](https://justineo.github.io/vue-clamp/demo/).
---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import vue from 'rollup-plugin-vue'
+import buble from 'rollup-plugin-buble'
+import { terser } from 'rollup-plugin-terser'
+import resolve from 'rollup-plugin-node-resolve'
+
+export default {
+  input: 'src/components/Clamp.js',
+  output: {
+    file: 'dist/vue-clamp.js',
+    name: 'VueClamp',
+    format: 'umd',
+    globals: {
+      vue: 'Vue'
+    }
+  },
+  external: [
+    'vue'
+  ],
+  plugins: [
+    resolve(),
+    vue({
+      compileTemplate: true
+    }),
+    buble(),
+    terser()
+  ]
+}
